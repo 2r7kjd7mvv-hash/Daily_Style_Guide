105579b6507cab1a4c34e13356174237dda2b342
杨扬 <yangyang@MacBook-Air.local>
2026-09-20 11:06:26 +0800

    test: exclude nested worktrees

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.worktrees/**'],
   },
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') },
